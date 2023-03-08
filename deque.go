2538e32944650eb520ba502deecd51014a214e58
Marcel-ICMC <marcelotoboni@gmail.com>
2023-03-08 06:59:16 -0300

    Index() Insert() and some other methods

--- deque.go
+++ deque.go
@@ -48,25 +48,94 @@ func (d *Deque) Count() int {
 	return d.Size
 }
 
-func (d *Deque) PrintList() {
-	n := (*d).Left
+func (d *Deque) Pop() int {
+	if d.Right != nil {
+		r := d.Right.Value
+
+		d.Right = d.Right.Left
+		d.Size--
+
+		if d.Right == nil {
+			d.Left = nil
+		} else {
+			d.Right.Right = nil
+		}
+		return r
+	}
+
+	panic("No nodes to pop!")
+}
+
+func (d *Deque) PopLeft() int {
+	if d.Left != nil {
+		r := d.Left.Value
+
+		d.Left = d.Left.Right
+		d.Size--
+
+		if d.Left == nil {
+			d.Right = nil
+		} else {
+			d.Left.Left = nil
+		}
+		return r
+	}
+
+	panic("No nodes to pop!")
+}
+
+func (d *Deque) Index(x int) int {
+	n := d.Left
+	for i := 0; n != nil; i++ {
+		if n.Value == x {
+			return i
+		}
+		n = n.Right
+	}
+
+	panic(fmt.Sprintf("There are no value %d in the deque", x))
+}
+
+func (d *Deque) Insert(i int, x int) {
+	if i > d.Size {
+		panic("Position unreachable!")
+	} else if i == 0 {
+		d.AppendLeft(x)
+		return
+	} else if i == d.Size {
+		d.Append(x)
+		return
+	}
+
+	n := d.Left
+	for j := 1; j < i; j++ {
+		n = n.Right
+	}
+
+	d.Size++
+	new := Node{Value: x, Left: n, Right: n.Right}
+	n.Right.Left = &new
+	n.Right = &new
+}
+
+func (d Deque) String() string {
+	n := d.Left
+	r := ""
 	for n != nil {
-		fmt.Printf("%d ", (*n).Value)
+		r += fmt.Sprintf("%d ", (*n).Value)
 		n = (*n).Right
 	}
 
-	fmt.Println()
+	return r
 }
 
 func main() {
 	t := Deque{}
 	t.Append(11)
 	t.Append(12)
-	t.Append(12)
-	t.Append(12)
-	t.Append(12)
-	t.AppendLeft(120)
-	t.AppendLeft(120)
-	t.AppendLeft(120)
-	t.PrintList()
+	t.Append(13)
+	t.Pop()
+	t.Insert(0, 0)
+	t.Insert(3, 1)
+	fmt.Printf("%s\n", t)
 }
